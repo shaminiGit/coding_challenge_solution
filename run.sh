1699a801a335ff0c1203a7d3c55ede2de5305556
Shamini <45043625+shaminiGit@users.noreply.github.com>
2020-04-12 13:13:39 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-#!/usr/bin/env bash
-# load dependancies
-apt-get update
-apt-get install python3.8 
-
-# set proper permissions
-chmod a+x ./src/consumer_complaints.py
-
-
-# execute scripts
-python3 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.txt
+python3.7 ./src/consumer_complaints.py ./input/complaints.csv ./output/report.csv
